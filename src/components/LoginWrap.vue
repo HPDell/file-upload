8fe1520bf154e4ee5ee9eac9613d886111d5cd93
whuls <hayzlsls@163.com>
2019-09-18 12:09:29 +0800

    v1.0.1 - if internet connected, geetest show, else hide

--- src/components/LoginWrap.vue
+++ src/components/LoginWrap.vue
@@ -22,9 +22,8 @@
             <el-form-item label="密码">
                 <el-input style="width:300px;" v-model="form.password" placeholder="密码" type="password"></el-input>
             </el-form-item>
-            <el-form-item label="完成验证">
-                <span v-if="isGtOk" id="gtModule"></span>
-                <span v-else>正在加载验证模块...</span>
+            <el-form-item v-if="isGtOk" label="完成验证">
+                <span id="gtModule"></span>
             </el-form-item>
         </el-form>
         <br>
@@ -61,72 +60,66 @@ export default {
         login(loginForm='loginForm') {
             const self = this;
             
-            if (!self.captchaObj) {
-                self.$alert('请先完成验证', '提示', {
-                    type: 'warning'
-                });
-                return;
-            }
-
             // 极验验证
-            let valid = self.captchaObj.getValidate();
-            if (valid) {
-
-                // 表单提交
-                self.$refs.loginForm.validate()
-                .then(valid => {
-                    if (valid) {
-                        self.$axios.post('/api/user/login', self.form)
-                        .then(response => {
-                            const data = response.data;
-                            if (data.status === 200) {
-                                self.$message({
-                                    message: data.message,
-                                    type: 'success'
-                                });
-
-                                // 设置用户权限
-                                sessionStorage.setItem('permission', data.permission);
-
-                                // 分权限浏览页面
-                                if (data.permission === 'user') {
-                                    self.$router.push('/user');
-                                    localStorage.setItem('user_name', data.name);
-                                    localStorage.setItem('user_account', self.form.account);
-                                } else if (data.permission === 'admin') {
-                                    self.$router.push('/admin');
-                                    localStorage.setItem('user_name', data.name);
-                                    localStorage.setItem('user_account', self.form.account);
-                                }
-                            } else {
-                                self.$message({
-                                    message: '[' + data.status + ']' + data.message,
-                                    type: 'error'
-                                });
-                            }
-                        })
-                        .catch(err => {
-                            if (err) {
-                                self.$message({
-                                    message: err,
-                                    type: 'error'
-                                });
-                                console.error(err);
-                            }
-                        })
-                    }
-                })
-                .catch(valid => {
-                    if (!valid) {
-                        console.error(valid);
-                    }
-                })
-
+            if (self.isGtOk && self.captchaObj) {
+                if (!self.captchaObj.getValidate()) {
+                    return self.$alert('请先完成验证', '提示', {
+                        type: 'warning'
+                    });
+                } else {
+                    self.submitForm();
+                }
             } else {
-                self.$alert('请先完成验证', '提示', {
-                    type: 'warning'
-                });
-            }
+                self.submitForm();
+            }            
+        },
+        submitForm() {
+            const self = this;
+            // 表单提交
+            self.$refs.loginForm.validate()
+            .then(valid => {
+                if (valid) {
+                    self.$axios.post('/api/user/login', self.form)
+                    .then(response => {
+                        const data = response.data;
+                        if (data.status === 200) {
+                            self.$message({
+                                message: data.message,
+                                type: 'success'
+                            });
+
+                            // 设置用户权限
+                            sessionStorage.setItem('permission', data.permission);
+
+                            // 分权限浏览页面
+                            if (data.permission === 'user') {
+                                self.$router.push('/user');
+                                localStorage.setItem('user_name', data.name);
+                                localStorage.setItem('user_account', self.form.account);
+                            } else if (data.permission === 'admin') {
+                                self.$router.push('/admin');
+                                localStorage.setItem('user_name', data.name);
+                                localStorage.setItem('user_account', self.form.account);
+                            }
+                        } else {
+                            self.$message({
+                                message: '[' + data.status + ']' + data.message,
+                                type: 'error'
+                            });
+                        }
+                    })
+                    .catch(err => {
+                        if (err) {
+                            self.$message({
+                                message: err,
+                                type: 'error'
+                            });
+                            console.error(err);
+                        }
+                    })
+                }
+            })
+            .catch(valid => console.error(valid) );
         },
         register() {
             this.$emit('register');
